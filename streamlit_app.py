d9abbf1716ef8f4da01076eea94adaf1d2740b77
santagia91 <giacomo.santarelli@youbidata.com>
2024-05-22 11:52:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,32 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Genera dati casuali
+data = pd.DataFrame({
+    'x': np.arange(1, 101),
+    'y': np.random.randn(100).cumsum()
+})
+
+# Titolo dell'applicazione
+st.title('Semplice Applicazione Streamlit')
+
+# Descrizione
+st.write('Questo è un semplice esempio di applicazione Streamlit che visualizza un grafico a linee.')
+
+# Slider per filtrare i dati
+min_value = st.slider('Seleziona il valore minimo di X', min_value=1, max_value=100, value=1)
+max_value = st.slider('Seleziona il valore massimo di X', min_value=1, max_value=100, value=100)
+
+# Filtra i dati
+filtered_data = data[(data['x'] >= min_value) & (data['x'] <= max_value)]
+
+# Visualizza i dati filtrati
+st.line_chart(filtered_data.set_index('x'))
+
+# Grafico con Matplotlib
+fig, ax = plt.subplots()
+ax.plot(filtered_data['x'], filtered_data['y'])
+st.pyplot(fig)
 
